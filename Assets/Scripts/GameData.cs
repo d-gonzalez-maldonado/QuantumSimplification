e1dab41159e90ed74f9547bf65645e312ca3b6c4
David GM <dagm@uchicago.edu>
2022-04-10 19:29:58 -0500

    latest version with working sendData

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -1,16 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.InteropServices;
 
 public static class GameData
 {
+
+
     public static int CurrLevel { get; set; } = 0;
     public static int max_level = 32;
     public static bool[] completedLevels = new bool[Constants.N_LEVELS];
 
     private static bool tutorialShown = false;
-    public static string getNextScene() {
 
+
+    [DllImport("__Internal")]
+    private static extern void SendData(string callback);
+
+    public static string getNextScene() {
+        string outString = "";
+        foreach (var score in completedLevels)
+        {
+            outString += score ? "1" : "0";
+        }
+#if UNITY_WEBGL == true && UNITY_EDITOR == false
+    SendData (outString);
+#endif
         if (tutorialShown) {
             tutorialShown = false;
             return "LevelScene";
